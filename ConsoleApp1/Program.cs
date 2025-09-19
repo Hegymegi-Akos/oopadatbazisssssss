ddeb72cb5e812f983f9eb31845ea99338436d507
Ákos <hegymegi-kissa@kkszki.hu>
2025-09-19 08:47:24 +0200

    Connect is ready to use.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace OOP_DB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MySqlConnection conn = new MySqlConnection("server=localhost; database=library; user=root; password=");
+            //try{
+            //    conn.Open();
+            //    Console.WriteLine("Sikeres csatlakozás!");
+            //}
+            //catch (Exception ex){
+            //    Console.WriteLine($"Csatlakozás sikertelen: {ex.Message}");
+            //}
+            //finally{
+            //    conn?.Close();
+            //}
+            Console.WriteLine("Kérem az adatbázis nevét: ");
+            string dbName = Console.ReadLine();
+            Connect conn = new Connect(dbName);
+        }
+    }
+}
