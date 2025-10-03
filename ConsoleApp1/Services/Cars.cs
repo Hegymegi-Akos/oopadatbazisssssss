0e4ce15534802d0963d61eeb85794072aeaf8550
Hegymegi-Akos <90569793+Hegymegi-Akos@users.noreply.github.com>
2025-10-03 08:34:48 +0200

    Cars.cs

--- ConsoleApp1/Services/Cars.cs
+++ ConsoleApp1/Services/Cars.cs
@@ -1,15 +1,59 @@
-﻿using OOP_DB;
-using System;
+using MySql.Data.MySqlClient;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1.Services
+namespace OOPAdatbazis.Services
 {
-    internal class Cars : ISqlStatement
+    internal class Cars : ISqlStatements
     {
-        public List<object> GetALlData(string dbName)
+        public object AddNewItem(object newRecord)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object DeleteItem(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<object> GetAllData(string dbName)
+        {
+            Connect conn = new Connect(dbName);
+
+            List<object> cars = new List<object>();
+
+            conn.Connection.Open();
+
+            string sql = "SELECT * FROM `cars`";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            dr.Read();
+
+            while (dr.Read())
+            {
+                var car = new
+                {
+                    id = dr.GetBodyDefinition(dr.GetName(0)),
+                    brand = dr.GetBodyDefinition(dr.GetName(1)),
+                    type = dr.GetBodyDefinition(dr.GetName(2)),
+                    mDate = dr.GetDateTime(dr.GetName(3))
+                };
+
+                cars.Add(car);
+            }
+            conn.Connection.Close();
+
+            return cars;
+        }
+
+        public object GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object UpdateIte(int id, object modifiedItem)
         {
             throw new NotImplementedException();
         }
