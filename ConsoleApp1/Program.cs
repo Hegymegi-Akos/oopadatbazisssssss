b30798472a124f22111501e2edae2f4037cee32f
Hegymegi-Akos <90569793+Hegymegi-Akos@users.noreply.github.com>
2025-10-03 08:33:27 +0200

    Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,43 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ConsoleApp1.Services;
-using MySql.Data.MySqlClient;
+using OOPAdatbazis.Services;
+using System;
 
-namespace OOP_DB
+namespace OOPAdatbazis
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //MySqlConnection conn = new MySqlConnection("server=localhost; database=library; user=root; password=");
-            //try{
-            //    conn.Open();
-            //    Console.WriteLine("Sikeres csatlakozás!");
-            //}
-            //catch (Exception ex){
-            //    Console.WriteLine($"Csatlakozás sikertelen: {ex.Message}");
-            //}
-            //finally{
-            //    conn?.Close();
-            //}
-            Console.WriteLine("Kérem az adatbázis nevét: ");
-            string dbName = Console.ReadLine();
-
-            ISqlStatement dataBase = new LIbrary();
 
 
-            
+            ISqlStatements dataBase = new Library();
+
+            Console.Write("Kérem a record id-jét:");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Kérem az uj title-t:");
+            string title = Console.ReadLine();
+
+            Console.Write("Kérem az uj author-t:");
+            string auhor = Console.ReadLine();
+
+            Console.Write("Kérem az uj releaseDate-et:");
+            string rdate = Console.ReadLine();
+
+            var book = new
+            {
+                Title = title,
+                Author = auhor,
+                ReleseDate = rdate
+            };
+
+            Console.WriteLine(dataBase.UpdateIte(id, book));
 
 
+            /*Console.Write("Kérem az datbázis nevét: ");
+            string dbName = Console.ReadLine();*/
 
-            foreach (var item in dataBase.GetALlData(dbName))
+            /*foreach (var item in dataBase.GetAllData(dbName))
             {
                 var books = item.GetType().GetProperties();
-                Console.WriteLine($"{books[0].Name}={books[0].GetValue(item)},{books[1].Name}={books[1].GetValue(item)}");
-            }
+                Console.WriteLine($"{books[0].Name}={books[0].GetValue(item)}, {books[1].Name}={books[1].GetValue(item)}");
+            }*/
+
+            /*Console.Write("Kérem a rekord id-jét: ");
+            Console.WriteLine(dataBase.GetById(int.Parse(Console.ReadLine())));*/
+
+            /*var book = new
+            {
+                title = "Star wars",
+                author = "Lucas",
+                releaseDate = "1980-01-01"
+            };
+
+            dataBase.AddNewItem(book);*/
+
+            //Console.WriteLine(dataBase.DeleteItem(101));
 
         }
     }
